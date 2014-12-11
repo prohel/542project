e1825a8d347c4f82a42491bfc22322fcd19e6415
Mark Mercurio <markmercurio@SEAS422.wlan.seas.upenn.edu>
2014-12-11 18:40:41 -0500

    Can now remove numbers/emails

--- server/LinkedList.c
+++ server/LinkedList.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "LinkedList.h"
 
 int addToList(node** pointerToHead, char* string) {
@@ -25,6 +26,47 @@ void printList(node* head) {
 		printf("%d. %s\n", i, current->string);
 	}
 }
+
+node* removeNode(node* head, char* string) {
+	printf("remove: %s\n", string);
+	
+	if (head == NULL) {
+		return NULL;
+	}
+	
+	
+	printf("strlen(head->string): %lu\n", strlen(head->string));
+	printf("strlen(string): %lu\n", strlen(string));
+	
+	printf("passed in: %s, head: %s\n", string, head->string);
+	printf("strcmp() evaluated to %s\n", strcmp(string, head->string) == 0 ? "TRUE" : "FALSE");
+	
+	if (strcmp(string, head->string) == 0) {
+		node* newHead = head->next;
+		free(head->string);
+		free(head);
+		return newHead;
+	}
+	
+	node* previous = head;
+	node* current = head->next;
+
+	while (current != NULL) {
+		
+		if (strcmp(string, current->string) == 0) {
+			previous->next = current->next;
+			free(current->string);
+			free(current);
+			return head;
+		}
+		
+		previous = current;
+		current = current->next;
+	}
+	
+	return head;
+}
+
 
 int freeList(node* head) {
 	
